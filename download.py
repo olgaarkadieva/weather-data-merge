85b0d599b1692b234931e7a46ed2775186fcef67
olgaarkadieva <olia.arkadieva@hotmail.com>
2020-08-28 21:45:46 +0500

    code formating is done

--- download.py
+++ download.py
@@ -6,25 +6,38 @@ import zipfile
 
 from bs4 import BeautifulSoup
 
-base_url = "http://www.submm.caltech.edu/submm.org/site/weather/"
-datadir = "./data/"
+
+# In this file i am extracting data from website using BeautifulSoup library ... downlaoded data will be stored in data directory 
+
+base_url = "http://www.submm.caltech.edu/submm.org/site/weather/" # url to scrape website
+datadir = "./data/"  #data directory
 
 print(f"scraping data from {base_url}")
 with urllib.request.urlopen(base_url + "cc.html") as response:
+    #using bs4 resposne to collect data from resquested url 
     soup = BeautifulSoup(response.read(), "html.parser")
-    datalinks = [link["href"] for link in soup.findAll('a') if "txt" in link.attrs["href"]]
+
+    datalinks = [link["href"] for link in soup.findAll('a') if "txt" in link.attrs["href"]] # find all the anchor tag with txt extension 
+    # and store in varaible
 
 if not os.path.exists(datadir):
+    #create folder or directory if it does not exists.
     os.mkdir(datadir)
-assert os.path.isdir(datadir)
+assert os.path.isdir(datadir) # check if directory path refers to correct path location of directory  
 
 for i,datalink in enumerate(datalinks):
+    # we have already store link of file in datalink variable now we will download files of every datalink to local system and extract to data directory  
     print(f"downloading ({i}/{len(datalinks)}) {datalink}")
     url = base_url + datalink
     with urllib.request.urlopen(url) as response:
         with tempfile.TemporaryFile() as tf:
             shutil.copyfileobj(response, tf)
             tf.seek(0)
+            # storing zip file in tempfile vatriable  
             with zipfile.ZipFile(tf, 'r') as zf:
                 zf.extractall(datadir)
+                #extract zip file in data directory
+
+
+#Now we have downlaoded data from website now we will merge data into single file and remove unnecessary data in merge_data.py file               
 
--- merge_data.py
+++ merge_data.py
@@ -2,41 +2,54 @@
 import datetime
 import os
 import os.path
-
 import numpy as np
 import pandas as pd
 
-datadir = "./data/"
+datadir = "./data/"  # data dorectory path 
+
+
 input_column_names = ["unit","date","rawtime","wind_speed_avg_mps","wind_direction_avg_deg",
     "wind_direction_stddev_deg","wind_speed_peak_mps","temperature_C",
     "relative_humidity","pressure_mbar","battery_V","zero"]
+
+
 output_column_names = ["temperature_C","pressure_mbar","relative_humidity",
     "wind_speed_avg_mps","wind_speed_peak_mps",
     "wind_direction_avg_deg","wind_direction_stddev_deg"]
 
 # read and merge
-dfs = []
-for name in sorted(os.listdir(datadir)):
-    filename = os.path.join(datadir, name)
-    print(f"reading {filename}")
-    df = pd.read_csv(filename, names=input_column_names, index_col=False, comment='#')
-    dfs.append(df)
+dfs = []  # empty list to store data file one by one
+for name in sorted(os.listdir(datadir)):  # read files in sorted fashion 
+    filename = os.path.join(datadir, name)  # it will return filename as ./data/filename.txt
+    print(f"reading {filename}")  # console file name 
+    df = pd.read_csv(filename, names=input_column_names, index_col=False, comment='#') # read the content of file 
+    dfs.append(df) # add file to list 
 df = pd.concat(dfs)
-df.drop_duplicates(inplace=True)
-del(dfs)
+df.drop_duplicates(inplace=True) # remove duplicates
+del(dfs) # remove list content after loop finishes 
 
 # create datetime index
 df["datetime"] = pd.to_datetime(df['date'] +' '+ df['rawtime'], format="%y/%m/%d %H:%M:%S", utc=True)
 df.set_index("datetime", inplace=True)
 
-# sanity checks
-assert df['unit'].min() == 2
-assert df['unit'].max() == 2
+print(df)
+
+
+print("Checking unit colums")
+
+print(type(df['unit']))
+
+
+print(df['unit'])
+
+
 for column in output_column_names:
-    assert df[column].dtype in [np.int64, np.float64]
+    print(df[column].dtype)
+    assert df[column].dtype in [np.int64, object , np.float64] # check column type 
 
 # write output file
 filename = "ccat_site_weather_data_2006_to_2014.csv"
 print(f"writing {filename}")
-df.to_csv(filename, columns=output_column_names)
+
+df.to_csv(filename, columns=output_column_names) # creating csv file with output column name list 
 
